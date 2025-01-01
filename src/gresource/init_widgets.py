edcff936a785014de553a63ca21bf8ab04a487df
maladziashyn <maladziashyn@gmail.com>
2025-01-01 22:40:28 +0100

    E-0 wip-1 run MC randnum

--- src/gresource/init_widgets.py
+++ src/gresource/init_widgets.py
@@ -1,11 +1,9 @@
 from gi.repository import GObject
 
 from gresource.monte_carlo import MonteCarloScrolledWindow
-from gresource.new_page import MyClass
 # from gresource.another_new_page import AnotherNewClass
 
 
 def load_widgets():
     GObject.type_ensure(MonteCarloScrolledWindow)
-    GObject.type_ensure(MyClass)
     # GObject.type_ensure(AnotherNewClass)
--- src/gresource/monte_carlo.py
+++ src/gresource/monte_carlo.py
@@ -7,8 +7,16 @@ from gi.repository import Gtk
 class MonteCarloScrolledWindow(Gtk.ScrolledWindow):
     __gtype_name__ = "MonteCarloScrolledWindow"
 
-    entry_mc_target_dir = Gtk.Template.Child()  # entry row
-    btn_target_dir = Gtk.Template.Child()
+    entry_target_dir = Gtk.Template.Child()  # entry row
+    spin_curves_count = Gtk.Template.Child()
+    spin_bins = Gtk.Template.Child()
+    spin_simulations_count = Gtk.Template.Child()
+    spin_ruin_level = Gtk.Template.Child()
+    spin_sim_trades = Gtk.Template.Child()
+    spin_sim_years = Gtk.Template.Child()
+    spin_sim_win_rate = Gtk.Template.Child()
+    spin_sim_win_loss_ratio = Gtk.Template.Child()
+    spin_sim_fraction = Gtk.Template.Child()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -18,10 +26,36 @@ class MonteCarloScrolledWindow(Gtk.ScrolledWindow):
         # self.en_directory.set_text(c.PROJ_SRC)
         # print(self.entry_mc_target_dir.get_text())
         native = Gtk.FileDialog()
-        native.select_folder(self, None, self._on_select_folder_complete)
+
+        # on select_folder: https://docs.gtk.org/gtk4/method.FileDialog.select_folder.html
+        native.select_folder(
+            None,  # self.btn_target_dir.get_ancestor(Gtk.Window),  # can be None
+            None,
+            self._on_select_folder_complete
+        )
 
     def _on_select_folder_complete(self, dialog, result):
         # folder = dialog.select_folder_finish(result)
         if (folder := dialog.select_folder_finish(result)):
             print(folder.get_path())
-            self.entry_mc_target_dir.set_text(folder.get_path())
+            self.entry_target_dir.set_text(folder.get_path())
+
+    @Gtk.Template.Callback("btn_callback_02")
+    def click_run_randnum(self, *args):
+        print("Clicked Run RANDNUM Simulation button")
+        print(self.entry_target_dir.get_text())
+        print(self.spin_curves_count.get_text())
+        print(self.spin_bins.get_text())
+        print(self.spin_simulations_count.get_text())
+        print(self.spin_ruin_level.get_text())
+        print(self.spin_sim_trades.get_text())
+        print(self.spin_sim_years.get_text())
+        print(self.spin_sim_win_rate.get_text())
+        print(self.spin_sim_win_loss_ratio.get_text())
+        print(self.spin_sim_fraction.get_text())
+
+        # # self.en_directory.set_text(c.PROJ_SRC)
+        # # print(self.entry_mc_target_dir.get_text())
+        # native = Gtk.FileDialog()
+        # # on select_folder: https://docs.gtk.org/gtk4/method.FileDialog.select_folder.html
+        # native.select_folder(self, None, self._on_select_folder_complete)
